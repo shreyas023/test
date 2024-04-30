2d9ce8c5a1c8bd7e7c12e4c8eec3134cda0a855d
shreyas023 <shreyas.bailkar023@nmims.edu.in>
2024-04-30 10:22:14 +0530

    routing sorted

--- index.js
+++ index.js
@@ -11,4 +11,24 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/public/contact.html");
+});
+
+app.get('/warranty', (req, res) => {    
+    res.sendFile(__dirname + "/public/warranty.html");
+});
+
+app.get('/blog', (req, res) => {
+    res.sendFile(__dirname + "/public/blog.html");
+});
+
+app.get('/matteppf', (req, res) => {
+    res.sendFile(__dirname + "/public/matteppf.html");
+});
+
+app.get('.clearppf', (req, res) => {
+    res.sendFile(__dirname + "/public/clearppf.html");
+});
+
 app.listen(port, () => console.log("Server listening on http://localhost:" + port));
